a117a5fa722fd5b443db20c211fb37aa12bb57ab
Michael Ben Yosef <mby735@gmail.com>
2016-12-05 20:28:24 +0200

    Refactor the routing a bit in preparation for message views. Remove the unnecessary 'users' route.

---
+++ routes/chats.js
@@ -0,0 +1,64 @@
+var models = require('../models');
+
+// Hard-coded for now:
+var username = 'alice';
+
+module.exports.index = function(req, res, next) {
+  res.render('chats', { title: 'Chat! - Index' });
+};
+
+module.exports.getChats = function(req, res, next) {
+    var query = models.Chat.find();
+    if (req.query.search) {
+        var regex = '/' + req.query.search + '/';
+        query = query.elemMatch('messages', { text: { $regex: regex }});
+    }
+    query.exec(function (err, chats) {
+        if (err) {
+            return next(err);
+        }
+        res.status(200);
+        var response = [];
+        for (var i = 0; i < chats.length; i++) {
+            var numMessages = chats[i].messages.length;
+            if (numMessages !== 0) {
+                var date = chats[i].messages[numMessages-1].date;
+            }
+            var viewed = chats[i].messagesViewed;
+            var read = viewed && viewed[username]
+            if (read === undefined) {
+                read = 0;
+            }
+            response.push({
+                url: '/chats/' + chats[i]._id,
+                topic: chats[i].topic,
+                startedBy: chats[i].startedBy,
+                lastMessageDate: date,
+                messages: numMessages,
+                unread: numMessages - read
+            });
+        }
+        res.json(response);
+    });
+};
+
+module.exports.postChat = function(req, res, next) {
+    var chat = new models.Chat({
+        topic: req.body.topic,
+        startedBy: username,
+        messagesViewed: {},
+        messages: []
+    });
+    
+    var error;
+    chat.save(function (err) {
+        error = err;
+    });
+
+    if (error) {
+        return next(error);
+    }
+
+    // TODO: Redirect to created chat.
+    res.redirect('/');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var chats = require('./chats');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chat! - Index' });
-});
+router.get('/', chats.index);
 
-// Hard-coded for now:
-var username = 'alice';
+router.get('/chats', chats.getChats);
 
-router.get('/chats', function(req, res, next) {
-    var query = models.Chat.find();
-    if (req.query.search) {
-        var regex = '/' + req.query.search + '/';
-        query = query.elemMatch('messages', { text: { $regex: regex }});
-    }
-    query.exec(function (err, chats) {
-        if (err) {
-            return next(err);
-        }
-        res.status(200);
-        var response = [];
-        for (var i = 0; i < chats.length; i++) {
-            var numMessages = chats[i].messages.length;
-            if (numMessages !== 0) {
-                var date = chats[i].messages[numMessages-1].date;
-            }
-            var viewed = chats[i].messagesViewed;
-            var read = viewed && viewed[username]
-            if (read === undefined) {
-                read = 0;
-            }
-            response.push({
-                url: '/chats/' + chats[i]._id,
-                topic: chats[i].topic,
-                startedBy: chats[i].startedBy,
-                lastMessageDate: date,
-                messages: numMessages,
-                unread: numMessages - read
-            });
-        }
-        res.json(response);
-    });
-});
-
-router.post('/chats', function(req, res, next) {
-    var chat = new models.Chat({
-        topic: req.body.topic,
-        startedBy: username,
-        messagesViewed: {},
-        messages: []
-    });
-    
-    var error;
-    chat.save(function (err) {
-        error = err;
-    });
-
-    if (error) {
-        return next(error);
-    }
-
-    // TODO: Redirect to created chat.
-    res.redirect('/');
-});
+router.post('/chats', chats.postChat);
     
 module.exports = router;
